1b475bdb551c07e44bca0ba2d540d353d46c7ee3
Ihor Berezhnyi <iberezhnyi@gmail.com>
2022-11-04 11:14:32 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,10 @@
 export function fetchCountries(name) {
   const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
 
-  return fetch(url).then(r => {
-    return r.json();
+  return fetch(url).then(res => {
+    if (!res.ok || res.status === 404) {
+      throw Error(onError);
+    }
+    return res.json();
   });
 }
